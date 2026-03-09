3ed4e0976be9f6e43252d27d862cdb8af91b05f7
devivaishnavi1510-boop <devi.vaishnavi1510@gmail.com>
2026-03-09 05:34:04 +0000

    arrays

---
+++ arrays.py
@@ -0,0 +1,8 @@
+
+numbers = [10, 20, 30, 40, 50]
+print("First element:", numbers[0])
+print("Third element:", numbers[2])
+numbers[1] = 25
+numbers.append(60)
+numbers.remove(30)
+print("Updated array:", numbers)
